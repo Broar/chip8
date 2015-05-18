334d6d900b6dd074bd08231c5fed1bb5a6c1afad
Steven Briggs <gshbriggs@gmail.com>
2015-05-17 20:59:09 -0400

    Fix indentation and spacing issues

--- python/cpu.py
+++ python/cpu.py
@@ -12,19 +12,19 @@ MEMORY = 4096
 REGISTERS = 16
 
 class CPU(object):
-	"""
+    """
 
-	A class representing the CHIP-8 CPU that uses the following specifications:
+    A class representing the CHIP-8 CPU that uses the following specifications:
 
     http://en.wikipedia.org/wiki/CHIP-8#Virtual_machine_description
     http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
 
-	"""
+    """
 
-	def __init__(self):
-		"""
+    def __init__(self):
+        """
 
-		Create a new CPU object for the CHIP-8 virtual machine.
+        Create a new CPU object for the CHIP-8 virtual machine.
 
         According to the specifications, the CHIP-8 CPU contains the following:
 
@@ -34,10 +34,10 @@ class CPU(object):
         * An address register called I
         * A program counter called PC
 
-		"""
+        """
 
         # TODO: Add remaining CPU components
-		self.memory = [0 for x in range(MEMORY)]
-		self.v = [0 for x in range(REGISTERS)]
-		self.i = 0
-		self.pc = 0
+        self.memory = [0 for x in range(MEMORY)]
+        self.v = [0 for x in range(REGISTERS)]
+        self.i = 0
+        self.pc = 0
